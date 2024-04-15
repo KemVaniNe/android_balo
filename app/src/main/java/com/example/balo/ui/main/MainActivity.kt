016715b17b63e43c1b487554b65a8113ab34c997
KemVaniNe <huynhthihongvan2703@gmail.com>
2024-04-15 20:18:42 +0700

    [VAN] bottom nav in main user

--- app/src/main/java/com/example/balo/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/balo/ui/main/MainActivity.kt
@@ -5,10 +5,17 @@ import com.example.balo.databinding.ActivityMainBinding
 import com.example.balo.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    private val viewPagerAdapter by lazy { MainViewPagerAdapter(supportFragmentManager) }
+
     override fun viewBinding(inflate: LayoutInflater): ActivityMainBinding =
         ActivityMainBinding.inflate(inflate)
 
     override fun initView() {
+        binding.viewPager.run {
+            offscreenPageLimit = 3
+            adapter = viewPagerAdapter
+        }
     }
 
     override fun initData() {
