592f991058874291bae6b89b76dc8ce1067a05ec
KemVaniNe <huynhthihongvan2703@gmail.com>
2024-04-15 20:09:30 +0700

    ui main

---
+++ app/src/main/java/com/example/balo/ui/main/NonSwipeViewPager.kt
@@ -0,0 +1,21 @@
+package com.example.balo.ui.main
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+class NonSwipeViewPager : ViewPager {
+    constructor(context: Context) : super(context) {}
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        return false
+    }
+}
