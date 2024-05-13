cf3a187e68de034f96e354d3da9d15204920fc74
KemVaniNe <huynhthihongvan2703@gmail.com>
2024-05-13 21:46:48 +0700

    [VAN] show list brands in home

--- app/src/main/java/com/example/balo/ui/user/home/HomeFragment.kt
+++ app/src/main/java/com/example/balo/ui/user/home/HomeFragment.kt
@@ -1,14 +1,21 @@
 package com.example.balo.ui.user.home
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.balo.R
 import com.example.balo.databinding.FragmentHomeBinding
 import com.example.balo.ui.base.BaseFragment
 import com.example.balo.adapter.BannerViewPagerAdapter
+import com.example.balo.adapter.brand.BrandAdapter
+import com.example.balo.data.model.BrandEntity
 import com.example.balo.ui.user.search.SearchActivity
+import com.example.balo.utils.Utils
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
@@ -16,13 +23,27 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private val banners = mutableListOf<Int>()
     private val viewPagerAdapter by lazy { BannerViewPagerAdapter(banners) }
     private lateinit var viewModel: HomeViewModel
+    private lateinit var dialog: AlertDialog
+    private val brands = mutableListOf<BrandEntity>()
+    private val brandAdapter by lazy {
+        BrandAdapter(brands) { pos ->
+            //TODO
+        }
+    }
+
     override fun initView() = binding.run {
         viewPager.adapter = viewPagerAdapter
         dotsIndicator.setViewPager(viewPager)
+        rvBrand.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        rvBrand.adapter = brandAdapter
     }
 
     override fun initData() {
+        context?.let {
+            dialog = Utils.showProgressDialog(it)
+        }
         viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        getBrands()
         listenVM()
     }
 
@@ -37,6 +58,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         container: ViewGroup?
     ): FragmentHomeBinding = FragmentHomeBinding.inflate(inflater)
 
+    @SuppressLint("NotifyDataSetChanged")
     private fun listenVM() {
         viewModel.banner.onEach {
             banners.run {
@@ -45,6 +67,24 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
             }
             viewPagerAdapter.notifyDataSetChanged()
         }.launchIn(scope = lifecycleScope)
+
+        viewModel.brands.observe(this) {
+            if(it != null) {
+                if(dialog.isShowing) dialog.dismiss()
+                brands.run {
+                    clear()
+                    addAll(it)
+                }
+                brandAdapter.notifyDataSetChanged()
+            }
+        }
     }
 
+    private fun getBrands() {
+        if (!dialog.isShowing) dialog.show()
+        viewModel.getAllBrands { error ->
+            if (dialog.isShowing) dialog.dismiss()
+            toast("${getString(R.string.error)}: ${error}. ${getString(R.string.try_again)}")
+        }
+    }
 }
--- app/src/main/java/com/example/balo/ui/user/home/HomeViewModel.kt
+++ app/src/main/java/com/example/balo/ui/user/home/HomeViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.balo.ui.user.home
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.balo.data.model.BrandEntity
+import com.example.balo.data.model.enum.Collection
 import com.example.balo.utils.Banner
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -11,5 +16,31 @@ class HomeViewModel : ViewModel() {
 
     init {
         _banners.tryEmit(Banner.banners)
+    }
+
+    private val _brands = MutableLiveData<List<BrandEntity>?>(null)
+    val brands = _brands
+
+    private val db = Firebase.firestore
+
+    fun getAllBrands(handleFail: (String) -> Unit) {
+        val data = mutableListOf<BrandEntity>()
+        db.collection(Collection.BRAND.collectionName)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val brand = BrandEntity(
+                        id = document.id,
+                        name = document.getString("name") ?: "",
+                        des = document.getString("des") ?: "",
+                        pic = document.getString("pic") ?: ""
+                    )
+                    data.add(brand)
+                }
+                _brands.postValue(data)
+            }
+            .addOnFailureListener { exception ->
+                handleFail(exception.message.toString())
+            }
     }
 }
