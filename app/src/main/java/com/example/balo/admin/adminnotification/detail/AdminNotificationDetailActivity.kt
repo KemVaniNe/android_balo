60ed4ca04ea8bb57a66e0ae3872cc0ded117c43b
KemVaniNe <huynhthihongvan2703@gmail.com>
2024-06-12 04:06:26 +0700

    [VAN] ui detail notification

---
+++ app/src/main/java/com/example/balo/admin/adminnotification/detail/AdminNotificationDetailActivity.kt
@@ -0,0 +1,19 @@
+package com.example.balo.admin.adminnotification.detail
+
+import android.view.LayoutInflater
+import com.example.balo.databinding.ActivityAdminNotificationDetailBinding
+import com.example.balo.shareview.base.BaseActivity
+
+class AdminNotificationDetailActivity : BaseActivity<ActivityAdminNotificationDetailBinding>() {
+    override fun viewBinding(inflate: LayoutInflater): ActivityAdminNotificationDetailBinding =
+        ActivityAdminNotificationDetailBinding.inflate(inflate)
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun initListener() {
+    }
+}
---
+++ app/src/main/java/com/example/balo/client/clientnotification/detail/ClientDetailNotificationActivity.kt
@@ -0,0 +1,19 @@
+package com.example.balo.client.clientnotification.detail
+
+import android.view.LayoutInflater
+import com.example.balo.databinding.ActivityClientDetailNotificationBinding
+import com.example.balo.shareview.base.BaseActivity
+
+class ClientDetailNotificationActivity : BaseActivity<ActivityClientDetailNotificationBinding>() {
+    override fun viewBinding(inflate: LayoutInflater): ActivityClientDetailNotificationBinding =
+        ActivityClientDetailNotificationBinding.inflate(inflate)
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun initListener() {
+    }
+}
