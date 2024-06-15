17bf9bb29d9e1287abc54e7d785cb8b8d34190cb
KemVaniNe <huynhthihongvan2703@gmail.com>
2024-06-15 10:42:30 +0700

    [VAN} search in chatbot

--- app/src/main/java/com/example/balo/client/chatbot/ChatbotVM.kt
+++ app/src/main/java/com/example/balo/client/chatbot/ChatbotVM.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.balo.data.model.ChatEntity
 import com.example.balo.data.model.ChatbotRequest
 import com.example.balo.data.network.ChatbotService
+import com.example.balo.data.network.ProductFirebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -24,6 +25,8 @@ class ChatbotVM : ViewModel() {
     private val chatbotService = ChatbotService()
 
     private val chatbotAPI = chatbotService.getChatbotAPI()
+
+    private val productFirebase = ProductFirebase()
 
     fun postMess(message: String, handleFail: (String) -> Unit) {
         val list = mutableListOf<ChatEntity>()
@@ -45,13 +48,53 @@ class ChatbotVM : ViewModel() {
                 val response = withContext(Dispatchers.IO) {
                     chatbotAPI.sendMessage(request)
                 }
-                list.add(ChatEntity(false, response.response))
-                _isLoading.postValue(false)
-                _mess.postValue(list)
+
+                if (extractVariable(response.response) == "SANPHAM") {
+                    getProducts(list, response.response, handleFail)
+                } else {
+                    list.add(ChatEntity(false, response.response))
+                    _isLoading.postValue(false)
+                    _mess.postValue(list)
+                }
             } catch (e: Exception) {
                 _isLoading.postValue(false)
                 handleFail.invoke("ERROR ${e.message}")
             }
+        }
+    }
+
+    private fun getProducts(
+        list: MutableList<ChatEntity>,
+        response: String,
+        handleFail: (String) -> Unit
+    ) {
+        productFirebase.getProducts(
+            handleSuccess = { products ->
+                val topSell = products.maxByOrNull { it.sell }
+                val newMess = if (topSell != null) {
+                    response.replace("1${extractVariable(response)}1_", "") + topSell.name
+                } else response
+                list.add(ChatEntity(false, newMess))
+                _mess.postValue(list)
+                _isLoading.postValue(false)
+            },
+            handleFail = {
+                _isLoading.postValue(false)
+                handleFail.invoke(it)
+            }
+        )
+    }
+
+    private fun extractVariable(input: String): String {
+        val startMarker = "1"
+        val endMarker = "1"
+        val startIndex = input.indexOf(startMarker)
+        val endIndex = input.indexOf(endMarker, startIndex + startMarker.length)
+
+        return if (startIndex != -1 && endIndex != -1 && startIndex < endIndex) {
+            input.substring(startIndex + 1, endIndex)
+        } else {
+            ""
         }
     }
 }
